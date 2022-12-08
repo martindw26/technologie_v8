2b19f9b392b37b929bde7b79dab13e9c943a79cb
martin williams <martindw97@gmail.com>
2022-12-08 22:40:08 +0000

    added padding leaderboard top

--- includes/section-top_header_slot_ad.php
+++ includes/section-top_header_slot_ad.php
@@ -1,4 +1,4 @@
-<div class="commerce_hp_desktop">
+<div class="commerce_hp_desktop pt-4">
 <div class="commerce_ldr rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto; background-color: #dee2e6;">
 <div><center>
 <p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
