d4eb981502e86ee679d8a1619569b026c8adbbc9
martin williams <martindw97@gmail.com>
2022-12-09 18:30:17 +0000

    sticky-top

--- template-parts/reviews_sidebar.php
+++ template-parts/reviews_sidebar.php
@@ -60,7 +60,7 @@ while ($block1->have_posts()) : $block1->the_post();?>
 <?php endwhile;  else :  endif; wp_reset_postdata();?>
 
 
-<div class="sticky">
+<div class="sticky-top">
 <div class="Commercial">
 	<div class="MPU_bottom">
 				<!-- MPU ad adslot-->
