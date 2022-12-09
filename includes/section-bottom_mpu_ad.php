8c8f9b4bb55f14bd66d6ead188b75f144931fc68
martin williams <martindw97@gmail.com>
2022-12-09 18:52:55 +0000

    text-dark

--- includes/section-bottom_mpu_ad.php
+++ includes/section-bottom_mpu_ad.php
@@ -1,7 +1,7 @@
 <div class="">
 <div class="commerce_ldr rounded rounded-1 mb-4" style="height:370px;  margin: 0 auto; background-color: #dee2e6;">
 <div><center>
-<p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
+<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
 <?php 
 
 $MPU_Bottom  = get_field( 'mpu_bottom_slot_body_script','option' );
