d4e527b299f2f9f7646bc99472f111eda35996f2
martin williams <martindw97@gmail.com>
2022-12-11 14:47:01 +0000

    update adslot responsive

--- includes/section-middle_header_ad.php
+++ includes/section-middle_header_ad.php
@@ -1,4 +1,5 @@
 <div class="commerce_hp_desktop">
+<div class="commerce_ldr">
 <div class="py-2">  
 <div class="d-flex justify-content-center p-2 text-dark" style="width:980px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
 <div class="d-flex align-items-center justify-content-center" style="height:270px; width:980px; margin: 0 auto; background-color: #dee2e6;">
@@ -6,19 +7,22 @@
 $Header_Middle_Slot = get_field( 'header_ad_middle_slot_body_script','option' );
 echo $Header_Middle_Slot;
 ?>
+</div>
 </div>
 </div>
 </div>
 
 
 <div class="commerce_hp_mobile">
-<div class="py-2">  
+<div class="commerce_ldr">
+<div class="py-2">
 <div class="d-flex justify-content-center p-2 text-dark" style="width:980px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
 <div class="d-flex align-items-center justify-content-center" style="height:270px; width:980px; margin: 0 auto; background-color: #dee2e6;">
 <?php
 $Header_Middle_Slot = get_field( 'header_ad_middle_slot_body_script','option' );
 echo $Header_Middle_Slot;
 ?>
+</div>
 </div>
 </div>
 </div>
