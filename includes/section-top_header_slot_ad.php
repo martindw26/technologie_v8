d84f3f7c2cad9b07471403cc2081db5d62f486d1
martin williams <martindw97@gmail.com>
2022-12-11 14:51:09 +0000

    ad update styling v2

--- includes/section-top_header_slot_ad.php
+++ includes/section-top_header_slot_ad.php
@@ -1,29 +1,28 @@
-<div class="commerce_hp_desktop pt-4">
-<div class="commerce_ldr rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto; background-color: #dee2e6;">
-<div><center>
-<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
-<?php 
-
+<div class="commerce_hp_desktop">
+<div class="commerce_ldr">
+<div class="py-2">  
+<div class="d-flex justify-content-center p-2 text-dark" style="width:980px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
+<div class="d-flex align-items-center justify-content-center" style="height:270px; width:980px; margin: 0 auto; background-color: #dee2e6;">
+<?php
 $Header_Top_Slot  = get_field( 'header_ad_slot','option' );
 echo $Header_Top_Slot;
-
 ?>
+</div>
+</div>
+</div>
+</div>
 
-</center></div> 
-</div>
-</div>
 
 <div class="commerce_hp_mobile">
-<div class="commerce_mobile rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto; background-color: #dee2e6;">
-<div><center>
-<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
-<?php 
-
+<div class="commerce_ldr">
+<div class="py-2">
+<div class="d-flex justify-content-center p-2 text-dark" style="width:350px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
+<div class="d-flex align-items-center justify-content-center" style="height:70px; width:350px; margin: 0 auto; background-color: #dee2e6;">
+<?php
 $Header_Top_Slot  = get_field( 'header_ad_slot','option' );
 echo $Header_Top_Slot;
-
 ?>
-
-</center></div> 
+</div>
+</div>
 </div>
 </div>
