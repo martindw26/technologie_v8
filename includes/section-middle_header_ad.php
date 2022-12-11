8613eb8222fb0835927584857d5c5631cd661bd9
martin williams <martindw97@gmail.com>
2022-12-11 14:43:28 +0000

    ad update styling v2

--- includes/section-middle_header_ad.php
+++ includes/section-middle_header_ad.php
@@ -1,28 +1,28 @@
 <div class="commerce_hp_desktop">
-<div class="commerce_ldr rounded rounded-1 mb-4" style="height:280px; width:980px; margin: 0 auto; background-color: #dee2e6;">
-<div>
-<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
-<?php 
-echo '<div class="rounded mx-auto d-block">';
+<div class="py-2">  
+<div class="d-flex justify-content-center p-2 text-dark" style="width:980px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
+<div class="d-flex align-items-center justify-content-center" style="height:270px; width:980px; margin: 0 auto; background-color: #dee2e6;">
+<?php
 $Header_Middle_Slot = get_field( 'header_ad_middle_slot_body_script','option' );
 echo $Header_Middle_Slot;
-echo '</div>';
 ?>
-</div> 
 </div>
 </div>
+</div>
+
 
 <div class="commerce_hp_mobile">
-<div class="commerce_mobile rounded rounded-1 mb-4" style="height:120px; width:370px; margin: 0 auto; background-color: #dee2e6;">
-<div><center>
-<p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
-<?php 
-echo '<div class="rounded mx-auto d-block">';
+<div class="py-2">  
+<div class="d-flex justify-content-center p-2 text-dark" style="width:980px; margin: 0 auto; background-color: #dee2e6;">Advertisment</div>
+<div class="d-flex align-items-center justify-content-center" style="height:270px; width:980px; margin: 0 auto; background-color: #dee2e6;">
+<?php
 $Header_Middle_Slot = get_field( 'header_ad_middle_slot_body_script','option' );
 echo $Header_Middle_Slot;
-echo '</div>';
 ?>
+</div>
+</div>
+</div>
 
-</center></div> 
-</div>
-</div>
+
+
+
