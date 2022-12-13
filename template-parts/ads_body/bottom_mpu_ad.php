e81066ca54b0ed6305c5e682dfd28e844cae4dca
martin williams <martindw97@gmail.com>
2022-12-13 19:25:44 +0000

    uniform MPU protected space background update

--- template-parts/ads_body/bottom_mpu_ad.php
+++ template-parts/ads_body/bottom_mpu_ad.php
@@ -1,4 +1,4 @@
-<div class="">
+<div>
 <div class="commerce_ldr rounded rounded-1 mb-4" style="height:370px;  margin: 0 auto; background-color: #dee2e6;">
 <div><center>
 <p class="d-flex justify-content-center pt-2 text-dark">Advertisment</p>
--- template-parts/ads_body/top_mpu_ad.php
+++ template-parts/ads_body/top_mpu_ad.php
@@ -1,5 +1,5 @@
-<div class="bg-light">
-<div class="commerce_ldr bg-secondary rounded rounded-1 mb-4" style="height:370px;  margin: 0 auto;">
+<div>
+<div class="commerce_ldr rounded rounded-1 mb-4" style="height:370px;  margin: 0 auto; background-color: #dee2e6;">
 <div><center>
 <p class="d-flex justify-content-center pt-2 text-light">Advertisment</p>
 <?php 
