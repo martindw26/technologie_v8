670839b6916eaab5e01ce6ec8f90d252b5ac8314
martin williams <martindw97@gmail.com>
2022-12-13 18:38:12 +0000

    locate_template

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ for (i = 0; i < coll.length; i++) {
 </script>
 
 <!-- Ad head scripts-->
-<?php get_template_part('theme/components','adshead');?>
+<?php locate_template('theme/components','adshead');?>
 <!-- END MPU Bottom ad head script -->
 
 
