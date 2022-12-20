c6848a1bd5ca74585c9d599788b2438176a45830
martin williams <86750566+martindw26@users.noreply.github.com>
2022-12-20 14:16:23 +0000

    Update adshead.php
    v8.4 added hide fields if empty

--- template-parts/theme/components/adshead.php
+++ template-parts/theme/components/adshead.php
@@ -1,21 +1,31 @@
 <!-- MPU Top ad head script -->
+<?php if( get_field( 'mpu_top_slot_head_script','option' ) ): ?>
 <?php the_field( 'mpu_top_slot_head_script','option' ); ?>
+<?php endif; ?>
 <!-- END MPU Top ad head script -->
 
 <!-- MPU Bottom ad head script -->
+<?php if( get_field( 'mpu_bottom_slot_head_script','option' ) ): ?>
 <?php the_field( 'mpu_bottom_slot_head_script','option' ); ?>
+<?php endif; ?>
 <!-- END MPU Bottom ad head script -->
 
 <!-- Homepage top ad head script -->
+<?php if( get_field( 'header_ad_top_slot_head','option' ) ): ?>
 <?php the_field( 'header_ad_top_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage top ad head script -->
 
 <!-- Homepage middle ad head script -->
+<?php if( get_field( 'header_ad_middle_slot_head','option' ) ): ?>
 <?php the_field( 'header_ad_middle_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage middle ad head script -->
 
 <!-- Homepage bottom ad head script -->
+<?php if( get_field( 'header_ad_bottom_slot_head','option' ) ): ?>
 <?php the_field( 'header_ad_bottom_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage bottom ad head script -->
 
 
