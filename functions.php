febe4a114f3c6b9aaa1163a0e7eea40e1bf5accc
martin williams <martindw97@gmail.com>
2022-12-20 19:25:58 +0000

    v8.4 - Theme functions

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
+// Theme functions
 
 include get_template_directory() . 'includes\functions\enqueue.php'; // Enqueue
 include get_template_directory() . 'includes\functions\thumbnails.php'; // Thumbnails
 include get_template_directory() . 'includes\functions\excerpt.php'; // Excerpt charater count limit
 include get_template_directory() . 'includes\functions\theme-support.php'; // Theme support
 include get_template_directory() . 'includes\functions\menu-function.php'; // Register Menus
-
 
 ?>
