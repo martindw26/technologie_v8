aefc19291160a5d5b74efd48274070a3ca8c07ec
Martin Williams <martindw97@gmail.com>
2022-12-20 01:15:47 +0000

    v8.4 Update

--- includes/section-non_featured_projects.php
+++ includes/section-non_featured_projects.php
@@ -1,5 +1,6 @@
 <div class="container">
-	<h2 class=" post_block_title text-dark">Latest <i>Projects</i></h2>
+	<?php $Block2Title = get_field ( 'block_2_title' );?>
+	<h2 class=" post_block_title text-dark">Latest <i><?php echo $Block2Title;?></i></h2>
 </div>
 <div class="container pt-lg-4 pb-lg-4">
 	<div class="row">
@@ -14,20 +15,27 @@
 		echo '<p>No content found</p>';
 		endif; ?>
 		<?php // Right small block posts projects loop begins here
-                                          $condition = array(
+		$category2 = get_field ( 'block_2_category' );
+											$condition3 = array(
                                                 "post_type"           => "post",
                                                 "post_status"         => "publish",
-                                                "orderby"             => "rand",
+                                                "orderby"             => "date",
                                                 "posts_per_page"      => 1,
-												'tag_id' 			=> 62,
-                                                'category__in' => array( 3 )
-                                          );                                           
-                                          $block1 = new WP_Query ($condition);
-                                          if ($block1->have_posts()) :
-                                          while ($block1->have_posts()) : $block1->the_post();?>
+												'tag__not_in' => array( 62),
+												'tax_query' => array(
+													array(
+													  'taxonomy' => 'category',
+													  'field'    => 'term_id', 
+													  'terms'    => $category2,
+													),
+												),
+												);                                           
+                                          $block4 = new WP_Query ($condition3);
+                                          if ($block4->have_posts()) :
+                                          while ($block4->have_posts()) : $block4->the_post();?>
 
 
-<div class="card rounded rounded-0 border border-0 shadow-sm p-3 mb-2 bg-body rounded" style="height:565px;"><!-- non-fetured block-->
+<div class="card rounded rounded-0 border border-0 shadow-sm p-3 mb-2 bg-body rounded" style="max-height:600px;"><!-- non-fetured block-->
 <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
 <img class="card-img-top rounded rounded-1 featured" src="<?php echo $url ?>" alt="Card image cap" style=" object-fit: cover; height:300px">  <div class="card-body">
     <h4 class="card-title fw-bold"><?php echo get_the_title();?></h4>
@@ -67,39 +75,46 @@
 		echo '<p>No content found</p>';
 		endif; ?>
 		<?php // Left block posts projects loop begins here
-                                          $condition = array(
-                                                "post_type"          => "post",
-                                                "post_status"        => "publish",
-                                                "orderby"            => "date",
-                                                "posts_per_page"     => 3,
-		              							'tag__not_in' => array( 62),
-                                                'category__in' => array( 3 )
-                                          );                                           
-                                          $block2 = new WP_Query ($condition);
-                                          if ($block2->have_posts()) :
-                                          while ($block2->have_posts()) : $block2->the_post();?>
-
+        $category2 = get_field ( 'block_2_category' );
+		$block2Offset = get_field ( 'block_2_right_offset' );
+		$condition4 = array(
+										"post_type"           => "post",
+										"post_status"         => "publish",
+										"orderby"             => "date",
+										"offset"             => $block2Offset,
+										"posts_per_page"      => 3,
+										'tax_query' => array(
+											array(
+											'taxonomy' => 'category',
+											'field'    => 'term_id', 
+											'terms'    => $category2,
+											),
+										),
+										);                                           
+								$block5 = new WP_Query ($condition4);
+								if ($block5->have_posts()) :
+								while ($block5->have_posts()) : $block5->the_post();?>
 <div class="non_featured_block_home_page">
 
 
-<div class="container shadow-sm p-3 mb-2 bg-body rounded"><!-- small non-fetured block-->
+<div class="container shadow-sm p-3 mb-2 bg-body rounded" style="height:195px;"><!-- small non-fetured block-->
 
 <div class="row">
 <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
   <div class="col"><img class="img-fluid featured rounded rounded-1" src="<?php echo $url ?>" style="height:150px";>
 </div>
     <div class="col-6">
-    <p class="card-title fw-bold"><?php echo get_the_title();?></p>
+    <h4 class="card-title fw-bold"><?php echo get_the_title();?></h4>
 	<p class="card-text text-muted">Article by: <?php echo get_the_author();?></i>,   Posted: <i><?php echo get_the_date();?></i><?php if($read_time):?> | Read time: <?php echo $read_time ?><?php endif ?></p>
 	<a href="<?php the_permalink() ?>" class="btn btn-sm btn-dark text-white m-0">Read More</a></div>
 
   </div>
 </div>
 </div>
-		<?php endwhile;  else :  endif; wp_reset_postdata();?>
+		<?php endwhile;  else :  endif; wp_reset_postdata();wp_reset_query();?>
 				</div>
 
-<div class="non_featured_block_home_page_mobile">
+
 
 <div class="col-lg-6">
 
@@ -110,19 +125,26 @@
 		echo '<p>No content found</p>';
 		endif; ?>
 		<?php // Left block posts projects loop begins here
-                                          $condition = array(
-                                                "post_type"           => "post",
-                                                "post_status"         => "publish",
-                                                "orderby"             => "date",
-                                                "posts_per_page"      => 3,
-		              							"offset"              => 1,
-		              							'tag__not_in' => array( 62),
-                                                'category__in' => array( 3 )
-                                          );                                           
-                                          $block2 = new WP_Query ($condition);
-                                          if ($block2->have_posts()) :
-                                          while ($block2->have_posts()) : $block2->the_post();?>
-
+         $category2 = get_field ( 'block_2_category' );
+		 $block2Offset = get_field ( 'block_2_right_offset' );
+		 $condition4 = array(
+										 "post_type"           => "post",
+										 "post_status"         => "publish",
+										 "orderby"             => "date",
+										 "offset"             => $block2Offset,
+										 "posts_per_page"      => 3,
+										 'tax_query' => array(
+											 array(
+											 'taxonomy' => 'category',
+											 'field'    => 'term_id', 
+											 'terms'    => $category2,
+											 ),
+										 ),
+										 );                                      
+                                          $block5 = new WP_Query ($condition4);
+                                          if ($block5->have_posts()) :
+                                          while ($block5->have_posts()) : $block5->the_post();?>
+<div class="non_featured_block_home_page_mobile">
 <div class="card rounded rounded-0 border border-0">
 <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
 <img class="card-img-top rounded rounded-0 featured" src="<?php echo $url ?>" alt="Card image cap" style=" object-fit: cover; height:300px">  <div class="card-body">
@@ -159,5 +181,6 @@
 	</div>
 </div><hr class="d-sm-block d-md-none">
 <div class="d-flex justify-content-center">
-<a href="https://techhobbyist.co.uk/category/reviews/" class="btn btn-white text-black border border-2 border-dark pb-sm-2 fs-5 text">More reviews</a>
+<?php $Block1MoreURL = get_field( 'block_2_more_url' );?>
+<a href="<?php echo $Block1MoreURL;?>" class="btn btn-white text-black border border-2 border-dark pb-sm-2 fs-5 text">More <?php echo $Block2Title;?></a>
 </div>
