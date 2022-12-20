35c87ca8ef5a372c071dde86df437055071e5531
martin williams <86750566+martindw26@users.noreply.github.com>
2022-12-20 14:43:11 +0000

    Update adshead.php
    v8.4 - debug ad issues

--- template-parts/theme/components/adshead.php
+++ template-parts/theme/components/adshead.php
@@ -13,39 +13,36 @@
 );
 ?>
 
-<!-- MPU Top ad head script -->
-	<?php if ( get_field( 'mpu_top_slot_head_script', 'options' )) : ?>
-		<?php echo wp_kses(get_field( 'mpu_top_slot_head_script', 'options' ), $allowed_html) ; ?>
-	<?php endif; ?>
-<!-- END MPU Top ad head script -->
 
-<!-- MPU Bottom ad head script -->
-	<?php if ( get_field( 'mpu_bottom_slot_head_script', 'options' )) : ?>
-		<?php echo wp_kses(get_field( 'mpu_bottom_slot_head_script', 'options' ), $allowed_html) ; ?>
-	<?php endif; ?>
-<!-- END MPU Bottom ad head script -->
-
-
-<!-- Homepage top ad head script-->
-	<?php if ( get_field( 'header_ad_top_slot_head', 'options' )) : ?>
-		<?php echo wp_kses(get_field( 'header_ad_top_slot_head', 'options' ), $allowed_html) ; ?>
-	<?php endif; ?>
+<!-- Homepage top ad head script -->
+<?php if( get_field( 'header_ad_top_slot_head','option' ), $allowed_html ): ?>
+<?php the_field( 'header_ad_top_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage top ad head script -->
 
-
 <!-- Homepage middle ad head script -->
-	<?php if ( get_field( 'header_ad_middle_slot_head', 'options' )) : ?>
-		<?php echo wp_kses(get_field( 'header_ad_middle_slot_head', 'options' ), $allowed_html) ; ?>
-	<?php endif; ?>
+<?php if( get_field( 'header_ad_middle_slot_head','option' ) ): ?>
+<?php the_field( 'header_ad_middle_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage middle ad head script -->
 
-
 <!-- Homepage bottom ad head script -->
-	<?php if ( get_field( 'header_ad_bottom_slot_head', 'options' )) : ?>
-		<?php echo wp_kses(get_field( 'header_ad_bottom_slot_head', 'options' ), $allowed_html) ; ?>
-	<?php endif; ?>
+<?php if( get_field( 'header_ad_bottom_slot_head','option' ) ): ?>
+<?php the_field( 'header_ad_bottom_slot_head','option' ); ?>
+<?php endif; ?>
 <!-- END Homepage bottom ad head script -->
 
+<!-- MPU top ad head script -->
+<?php if( get_field( 'mpu_top_slot_head_script','option' ) ): ?>
+<?php the_field( 'mpu_top_slot_head_script','option' ); ?>
+<?php endif; ?>
+<!-- END MPU top ad head script -->
+
+<!-- MPU bottom ad head script -->
+<?php if( get_field( 'mpu_bottom_slot_head_script','option' ) ): ?>
+<?php the_field( 'mpu_bottom_slot_head_script','option' ); ?>
+<?php endif; ?>
+<!-- END MPU bottom ad head script-->
 
 
               
