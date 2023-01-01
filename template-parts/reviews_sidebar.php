45ebcd984f24a288c5058c27103dc2e35ea64b32
martin <martindw97@gmail.com>
2023-01-01 22:40:35 +0000

    v8.4 - template-parts/ads_body/top/section/top_mpu_placement.php 1.1

--- template-parts/reviews_sidebar.php
+++ template-parts/reviews_sidebar.php
@@ -27,11 +27,10 @@ if ($block1->have_posts()) :
 while ($block1->have_posts()) : $block1->the_post();?>
 <!-- Blog post-->
 
-<!-- Header adslot-->
+<div class="col">
+	<!-- Header adslot-->
 <?php get_template_part( 'template-parts/ads_body/top/section/top_mpu_placement.php' );?>
 <!-- End Featured Post-->
-
-<div class="col">
  <div class="card rounded rounded-0 border border-1">
 <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
 <img class="card-img-top rounded rounded-0 featured" src="<?php echo $url ?>" alt="Card image cap" style=" object-fit: cover; height:300px">  <div class="card-body">
