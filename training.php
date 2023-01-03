fba330be388cd39aa2590b9a88925ffe0006efbc
martin <martindw97@gmail.com>
2023-01-03 19:52:44 +0000

    v8.4 - updated training post format

--- training.php
+++ training.php
@@ -1,9 +1,30 @@
 <?php
-/*Template name: Training*/
-?>
+  get_header();
+ /*Template name: Training*/
+ /* Template Post Type: post */
+?>  
 
-<?php get_header();?>
+<div class="container">
 
+        <div class="row">
+	
+          <div class="col-lg-8 p-lg-2 bg-white text-dark">
+
+
+            <h1 class=" display-2 font-weight-bold text-dark" style=""><?php echo esc_html( get_the_title() );?></h1>
+
+				<!-- Social share icons-->
+				<?php $enable_in_article = get_field('on_article','option');?>
+				<?php if($enable_in_article):?>
+					<?php get_template_part('includes/section','social_sharefrontpagearticle');?>
+				<?php else:?>
+					<?php echo '<style>{ display:none;}</style>';?>
+				<?php endif;?>
+				<!-- End Social share icons-->
+
+
+             <!-- header image block-->
+                                                
                         <!-- Header adslot-->
                         <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_top_slot_ad' );?>
                         <!-- End Featured Post-->
@@ -16,12 +37,37 @@
                         <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_middle_slot_ad' );?>
                         <!-- End Header middle adslot-->
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+             <!-- Social share icons-->
+             <?php $enable_in_article = get_field('on_article','option');?>
+             <?php if($enable_in_article):?>
+             <?php get_template_part('includes/section','social_sharefrontpagearticle');?>
+             <?php else:?>
+             <?php echo '<style>{ display:none;}</style>';?>
+             <?php endif;?>
+              <hr>
+             <!-- End Social share icons-->
 
-<?php get_footer();?>
+              	
+             <!-- Start of posts related posts-->
+             <?php get_template_part( 'template-parts/related_new' );?>
+             <!-- End of posts related posts-->
+
+          </div>
+
+                        <div class="sidebar col-lg-4 col-md-4">
+
+                                      <!-- Category sidebar search-->
+                                      <?php get_template_part('includes/section','search');?>
+                                      <!-- End Category sidebar search-->
+
+		   <!-- Start of posts sidebar related posts-->
+	 	   <?php get_template_part( 'template-parts/reviews_sidebar' );?>
+		   <!-- End of posts sidebar related posts-->                         
+	     </div>
+
+
+
+
+      </div>               
+
+</div>
