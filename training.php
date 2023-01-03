3f4fd299dcc7124cc66ef0c23c5a23132a124e6c
martin <martindw97@gmail.com>
2023-01-03 21:13:10 +0000

    v8.4 - updated training post format - v1.2 revert

--- training.php
+++ training.php
@@ -4,80 +4,24 @@
 
 <?php get_header();?>
 
-<div class="container">
+                        <!-- Header adslot-->
+                        <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_top_slot_ad' );?>
+                        <!-- End Featured Post-->
 
-<div class="row">
+                        <!-- Post Block 1-->
+                        <?php get_template_part('template-parts/block_2');?><hr class="container">
+                        <!-- End of Post Block--> 
 
-<div class="col-lg-8 p-lg-2">
+                        <!-- Header middle adslot-->
+                        <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_middle_slot_ad' );?>
+                        <!-- End Header middle adslot-->
 
-<?php if (have_posts()) : ?>
-
-<h2 class="p-2 text-dark"><?php
-if ( is_category() ) {
-single_cat_title('Latest ');
-} elseif ( is_tag() ) {
-single_cat_title('Latest tags: ');
-} elseif ( is_author() ) {
-the_post();
-echo 'Author Archives: ' . get_the_author();
-rewind_posts();
-} elseif ( is_day() ) {
-echo 'Daily Archives: ' . get_the_date();
-} elseif ( is_month() ) {
-echo 'Monthly Archives: ' . get_the_date('F Y');
-} elseif ( is_year() ) {
-echo 'Yearly Archives: ' . get_the_date('Y');
-} else {
-echo 'Archives:';
-}
-
-?></h2>
-
-<!-- SEO text-->
-
-<div class=" bg-secondary text-light p-4 mb-4 border-dark lead">
-<div><?php echo category_description(); ?></div> 
-</div>
-
-
-<?php while (have_posts()) : the_post(); ?>
-
-<div class="col-md">
-
-
-
-<div class="container mt-3 border-border border-0 shadow-sm p-3 mb-5 bg-body rounded">
-
-<div class="row"><!-- block left-->
-<!-- Post Block 1-->
-<?php get_template_part('template-parts/block_2');?><hr class="container">
-<!-- End of Post Block--> 
-<br>
-</div>
-</div>
-
-
-
-
-</div>	
-<?php endwhile; ?>
-<?php endif; wp_reset_postdata();?>
-
-</div>
-
-<div class="sidebar col-lg-4 col-md-4 p-2">
-
-<!-- Category sidebar search-->
-<?php get_template_part('includes/section','search');?>
-<!-- End Category sidebar search-->
-
-<!-- Start of posts sidebar related posts-->
-<?php get_template_part( 'template-parts/generic_sidebar' );?>
-<!-- End of posts sidebar related posts-->        
-</div>
-
-</div>               
-
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer();?>
