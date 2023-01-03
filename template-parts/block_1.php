c96379b0a88c1f9b42d3dd5455911dc9804d81a4
martin <martindw97@gmail.com>
2023-01-03 23:20:31 +0000

    v8.4 - template-parts/block_1 added sidebar v3.3

--- template-parts/block_1.php
+++ template-parts/block_1.php
@@ -6,8 +6,6 @@
 <div class="row">
 <div class="col-lg-8 p-lg-2">
 
-
-
 <?php if (have_posts()) : while (have_posts()) : the_post();
 		the_content();
 		endwhile;
@@ -46,6 +44,15 @@
 </div>
 
 <?php endwhile;  else :  endif; wp_reset_postdata();?>
+
+<!-- Category sidebar search-->
+<?php get_template_part('includes/section','search');?>
+<!-- End Category sidebar search-->
+
+<!-- Start of posts sidebar related posts-->
+<?php get_template_part( 'template-parts/generic_sidebar' );?>
+<!-- End of posts sidebar related posts-->        
+</div>
 
 <div class="col-lg-8 p-lg-2">
 
@@ -97,7 +104,7 @@
 		<?php endwhile;  else :  endif; wp_reset_postdata();wp_reset_query();?>
 				</div>
 
-<div class="sidebar col-lg-4 col-md-4 p-2">
+        <div class="sidebar col-lg-4 col-md-4 p-2">
 
 <!-- Category sidebar search-->
 <?php get_template_part('includes/section','search');?>
