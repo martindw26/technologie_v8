4d91fcf1973907e41fcbcb37bf5b41b8d27fae2b
martin <martindw97@gmail.com>
2023-01-03 21:33:22 +0000

    v8.4 - updated training post format - v2.1 revert

--- training.php
+++ training.php
@@ -1,56 +1,27 @@
 <?php
-  get_header();
 /*Template name: Training*/
-?>  
+?>
 
-<div class="container">
+<?php get_header();?>
 
-        <div class="row">
+                        <!-- Header adslot-->
+                        <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_top_slot_ad' );?>
+                        <!-- End Featured Post-->
 
-          <div class="col-lg-8 p-lg-2">
+                        <!-- Post Block 1-->
+                        <?php get_template_part('template-parts/block_2');?><hr class="container">
+                        <!-- End of Post Block--> 
 
-          <?php if (have_posts()) : ?>
+                        <!-- Header middle adslot-->
+                        <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_middle_slot_ad' );?>
+                        <!-- End Header middle adslot-->
 
-             <!-- SEO text-->
-
-             <div class=" bg-secondary text-light p-4 mb-4 border-dark lead">
-             	<div><?php echo category_description(); ?></div> 
-             </div>
-
-
-          <?php while (have_posts()) : the_post(); ?>
-
-        <div class="col-md">
-
-<div class="container mt-3 border-border border-0 shadow-sm p-3 mb-5 bg-body rounded">
-
-<!-- Post Block 1-->
-<?php get_template_part('template-parts/block_2');?><hr class="container">
-<!-- End of Post Block--> 
-
-<br>
-  </div>
-</div>
-
-        </div>	
-            <?php endwhile; ?>
-            <?php endif; wp_reset_postdata();?>
-
-          </div>
-
-                        <div class="sidebar col-lg-4 col-md-4 p-2">
-
-                                      <!-- Category sidebar search-->
-                                      <?php get_template_part('includes/section','search');?>
-                                      <!-- End Category sidebar search-->
-
-		   <!-- Start of posts sidebar related posts-->
-	 	   <?php get_template_part( 'template-parts/generic_sidebar' );?>
-		   <!-- End of posts sidebar related posts-->        
                         </div>
-
-      </div>               
-
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
