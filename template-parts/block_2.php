d43f4d8b18725bb580b479f5e1a3f690afdc2719
martin <martindw97@gmail.com>
2023-01-03 20:05:52 +0000

    v8.4 - updated training post format - v1.4

---
+++ template-parts/block_2.php
@@ -0,0 +1,51 @@
+<?php if (have_posts()) : while (have_posts()) : the_post();
+		the_content();
+		endwhile;
+		else :
+		echo '<p>No content found</p>';
+		endif; ?>
+		<?php // Left block posts projects loop begins here
+        $training_category1 = get_field ( 'training_block_1_category' );
+        $training_block_1_left_post_exclude = get_field ( 'training_block_1_left_post_exclude' );
+		$tblock1Offset = get_field ( 'tblock1Offset' );
+		$tcondition2 = array(
+                                    'orderby' => 'meta_value_num',
+                                    'order' => 'ASC',
+                                    'post_type' => 'post',
+                                    "post_status"    => "publish",
+                                    'posts_per_page' => 3,
+                                    'post__not_in' => $training_block_1_left_post_exclude,
+                                    'tax_query' => array(
+                                        array(
+                                        'taxonomy' => 'category',
+                                        'field'    => 'term_id', 
+                                        'terms'    => $training_category1
+                                        )
+                                    )
+										);                                           
+								$tblock2 = new WP_Query ($tcondition2);
+								if ($tblock2->have_posts()) :
+								while ($tblock2->have_posts()) : $tblock2->the_post();?>
+
+
+
+<div class="col-md">
+<div class="container mt-3 border-border border-0 shadow-sm p-3 mb-5 bg-body rounded">
+<div class="row"><!-- block left-->
+<div class="col-md-6 col-sm bg-white text-dark">
+<?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+<img class="img-fluid p-1 rounded-1" src="<?php echo $url ?>"/>
+</div><!-- End block right-->
+<div class="col-md-6 col-sm bg-white text-dark p-2"><!-- block right-->
+<h5 class="card-title"><?php echo get_the_title();?></h5>
+
+<p class="p-1 text-dark">&ldquo;<?php echo excerpt(15);?>&rdquo;</p>
+<p class="card-text"><small class="text-dark"><?php echo get_the_date();?></small></p>
+<a href="<?php the_permalink() ?>" class="btn btn-sm btn-dark text-white m-0">Read More</a>
+
+</div><!-- End block right-->
+
+<?php endwhile;  else :  endif; wp_reset_postdata();wp_reset_query();?>
+
+
+
--- training.php
+++ training.php
@@ -1,105 +1,27 @@
 <?php
-  get_header();
- /*Template name: Training*/
- /* Template Post Type: post */
-?>  
+/*Template name: Training*/
+?>
 
-<div class="container">
+<?php get_header();?>
 
-        <div class="row">
-
-        <div class="non_featured_block_home_page">
-	
-    <div class="col-lg-8 p-lg-2 bg-white text-dark">
-
-        <?php if (have_posts()) : while (have_posts()) : the_post();
-		the_content();
-		endwhile;
-		else :
-		echo '<p>No content found</p>';
-		endif; ?>
-		<?php // Left block posts projects loop begins here
-        $training_category1 = get_field ( 'training_block_1_category' );
-        $training_block_1_left_post_exclude = get_field ( 'training_block_1_left_post_exclude' );
-		$tblock1Offset = get_field ( 'tblock1Offset' );
-		$tcondition2 = array(
-                                    'orderby' => 'meta_value_num',
-                                    'order' => 'ASC',
-                                    'post_type' => 'post',
-                                    "post_status"    => "publish",
-                                    'posts_per_page' => 3,
-                                    'post__not_in' => $training_block_1_left_post_exclude,
-                                    'tax_query' => array(
-                                        array(
-                                        'taxonomy' => 'category',
-                                        'field'    => 'term_id', 
-                                        'terms'    => $training_category1
-                                        )
-                                    )
-										);                                           
-								$tblock2 = new WP_Query ($tcondition2);
-								if ($tblock2->have_posts()) :
-								while ($tblock2->have_posts()) : $tblock2->the_post();?>
-       
-
-
-            <h1 class=" display-2 font-weight-bold text-dark" style=""><?php echo esc_html( get_the_title() );?></h1>
-
-				<!-- Social share icons-->
-				<?php $enable_in_article = get_field('on_article','option');?>
-				<?php if($enable_in_article):?>
-					<?php get_template_part('includes/section','social_sharefrontpagearticle');?>
-				<?php else:?>
-					<?php echo '<style>{ display:none;}</style>';?>
-				<?php endif;?>
-				<!-- End Social share icons-->
-
-
-             <!-- header image block-->
-                                                
                         <!-- Header adslot-->
                         <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_top_slot_ad' );?>
                         <!-- End Featured Post-->
 
                         <!-- Post Block 1-->
-                        <?php get_template_part('template-parts/block_1');?><hr class="container">
+                        <?php get_template_part('template-parts/block_2');?><hr class="container">
                         <!-- End of Post Block--> 
 
                         <!-- Header middle adslot-->
                         <?php get_template_part( 'template-parts/landing_page_ads/homepage_header_middle_slot_ad' );?>
                         <!-- End Header middle adslot-->
 
-             <!-- Social share icons-->
-             <?php $enable_in_article = get_field('on_article','option');?>
-             <?php if($enable_in_article):?>
-             <?php get_template_part('includes/section','social_sharefrontpagearticle');?>
-             <?php else:?>
-             <?php echo '<style>{ display:none;}</style>';?>
-             <?php endif;?>
-              <hr>
-             <!-- End Social share icons-->
-             <?php endwhile;  else :  endif; wp_reset_postdata();wp_reset_query();?>
-              	
-             <!-- Start of posts related posts-->
-             <?php get_template_part( 'template-parts/related_new' );?>
-             <!-- End of posts related posts-->
-
-          </div>
-
-                        <div class="sidebar col-lg-4 col-md-4">
-
-                                      <!-- Category sidebar search-->
-                                      <?php get_template_part('includes/section','search');?>
-                                      <!-- End Category sidebar search-->
-
-		   <!-- Start of posts sidebar related posts-->
-	 	   <?php get_template_part( 'template-parts/reviews_sidebar' );?>
-		   <!-- End of posts sidebar related posts-->                         
-	     </div>
-
-
-
-
-      </div>               
-
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<?php get_footer();?>
