e7f29688cddc2d35e7d766a2d7fed63f3b3823e1
martin <martindw97@gmail.com>
2023-01-03 20:01:35 +0000

    v8.4 - updated training post format - v1.2

--- training.php
+++ training.php
@@ -7,6 +7,36 @@
 <div class="container">
 
         <div class="row">
+
+        <?php if (have_posts()) : while (have_posts()) : the_post();
+		the_content();
+		endwhile;
+		else :
+		echo '<p>No content found</p>';
+		endif; ?>
+		<?php // Left block posts projects loop begins here
+        $training_category1 = get_field ( 'training_block_1_category' );
+        $training_block_1_left_post_exclude = get_field ( 'training_block_1_left_post_exclude' );
+		$tblock1Offset = get_field ( 'tblock1Offset' );
+		$tcondition2 = array(
+                                    'orderby' => 'meta_value_num',
+                                    'order' => 'ASC',
+                                    'post_type' => 'post',
+                                    "post_status"    => "publish",
+                                    'posts_per_page' => 3,
+                                    'post__not_in' => $training_block_1_left_post_exclude,
+                                    'tax_query' => array(
+                                        array(
+                                        'taxonomy' => 'category',
+                                        'field'    => 'term_id', 
+                                        'terms'    => $training_category1
+                                        )
+                                    )
+										);                                           
+								$tblock2 = new WP_Query ($tcondition2);
+								if ($tblock2->have_posts()) :
+								while ($tblock2->have_posts()) : $tblock2->the_post();?>
+        <div class="non_featured_block_home_page">
 	
           <div class="col-lg-8 p-lg-2 bg-white text-dark">
 
@@ -46,7 +76,7 @@
              <?php endif;?>
               <hr>
              <!-- End Social share icons-->
-
+             <?php endwhile;  else :  endif; wp_reset_postdata();wp_reset_query();?>
               	
              <!-- Start of posts related posts-->
              <?php get_template_part( 'template-parts/related_new' );?>
