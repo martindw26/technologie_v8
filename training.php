8f08921d7a6124dbe8b341408a5c31fb9a182a0c
martin <martindw97@gmail.com>
2023-01-04 01:48:25 +0000

    v8.5 - block 2 added v2.3

--- training.php
+++ training.php
@@ -14,35 +14,35 @@
                             <?php get_template_part('template-parts/block_1');?>
                             <!-- End of Post Block--> 
                         <br>
-                        <h2 class="p-2 text-dark"><?php the_field( 'trainingblock1title' );?></h2>
-                            <!-- Post Block 1-->
-                            <?php get_template_part('template-parts/block_1');?>
-                            <!-- End of Post Block--> 
                     </div>
                 </div>
             </div>	
+
         </div>
 </div>
 
-</div> 
-</div> 
-</div> 
 
-</div> 
-</div> 
-</div> 
 
-</div> 
-</div> 
-</div> 
 
-<div class="sidebar col-lg-4 col-md-4 p-2">
-        <!-- Category sidebar search-->
-        <?php get_template_part('includes/section','search');?>
-        <!-- End Category sidebar search-->
-        <!-- Start of posts sidebar related posts-->
-        <?php get_template_part( 'template-parts/generic_sidebar' );?>
-        <!-- End of posts sidebar related posts-->        
-</div> 
+        </div>	
+            <?php endwhile; ?>
+            <?php endif; wp_reset_postdata();?>
+
+          </div>
+
+                        <div class="sidebar col-lg-4 col-md-4 p-2">
+
+                                      <!-- Category sidebar search-->
+                                      <?php get_template_part('includes/section','search');?>
+                                      <!-- End Category sidebar search-->
+
+		   <!-- Start of posts sidebar related posts-->
+	 	   <?php get_template_part( 'template-parts/generic_sidebar' );?>
+		   <!-- End of posts sidebar related posts-->        
+                        </div>
+
+      </div>               
+
+</div>
 
 <?php get_footer();?>
