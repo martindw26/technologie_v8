7aedd929bd4a6bf37815e0eae876205f3d57b9ca
martin <martindw97@gmail.com>
2023-01-04 00:46:56 +0000

    <h2 class="p-2 text-dark">Training</h2> v2.2

--- training.php
+++ training.php
@@ -5,12 +5,10 @@
 <?php get_header();?>
 
 <div class="container">
-
-<h2 class="p-2 text-dark">Training</h2>
-
         <div class="row">
             <div class="col-lg-8 p-lg-2">
                 <div class="col-md">
+                <h2 class="p-2 text-dark">Training</h2>
                     <div class="container mt-3 border-border border-0 p-3 rounded">
                             <!-- Post Block 1-->
                             <?php get_template_part('template-parts/block_2');?>
