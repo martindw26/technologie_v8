299d74abdf5b94ff5fb21454aeef790b59ca727c
martin <martindw97@gmail.com>
2023-01-04 01:18:22 +0000

    v8.5 - added block 2 v1.3

--- training.php
+++ training.php
@@ -16,15 +16,24 @@
                         <br>
                         <h2 class="p-2 text-dark"><?php the_field( 'trainingblock1title' );?></h2>
                             <!-- Post Block 1-->
-                            <?php get_template_part('template-parts/block_2');?>
+                            <?php get_template_part('template-parts/block_1');?>
                             <!-- End of Post Block--> 
-                    </div>
                     </div>
                 </div>
             </div>	
         </div>
 </div>
 
+<div class="container">
+        <div class="row">
+            <div class="col-lg-8 p-lg-2">
+                <div class="col-md">
+                    <div class="container mt-3 border-border border-0 p-3 rounded">
+
+                </div>
+            </div>	
+        </div>
+</div>
 
 <div class="sidebar col-lg-4 col-md-4 p-2">
         <!-- Category sidebar search-->
@@ -37,7 +46,6 @@
 
 </div> 
 </div> 
-</div>
 </div> 
 
 <?php get_footer();?>
