3c30323e7289d561d2bd85d5ebf7e96274f4e461
martin <martindw97@gmail.com>
2023-01-04 00:14:39 +0000

    v8.4 - template-parts/block_2 v1.3

--- training.php
+++ training.php
@@ -5,24 +5,18 @@
 <?php get_header();?>
 
 <div class="container">
-
-<div class="row">
-
-<div class="col-lg-8 p-lg-2">
-
-<div class="col-md">
-
-<div class="container mt-3 border-border border-0 shadow-sm p-3 mb-5 bg-body rounded">
-
-<!-- Post Block 1-->
-<?php get_template_part('template-parts/block_2');?>
-<!-- End of Post Block--> 
-<br>
-</div>
-</div>
-
-
-</div>	
+        <div class="row">
+            <div class="col-lg-8 p-lg-2">
+                <div class="col-md">
+                    <div class="container mt-3 border-border border-0 shadow-sm p-3 mb-5 bg-body rounded">
+                            <!-- Post Block 1-->
+                            <?php get_template_part('template-parts/block_2');?>
+                            <!-- End of Post Block--> 
+                        <br>
+                    </div>
+                </div>
+            </div>	
+        </div>
 </div>
 
 <div class="sidebar col-lg-4 col-md-4 p-2">
