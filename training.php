290ba4c0df1fc9b19e7e6966742967f3cd014c42
martin <martindw97@gmail.com>
2023-01-04 01:53:08 +0000

    v8.5 - block 2 added v2.7

--- training.php
+++ training.php
@@ -13,20 +13,7 @@
                             <?php get_template_part('template-parts/block_1');?>
                             <!-- End of Post Block--> 
                         <br>
-                    </div>
-                </div>
-            </div>	
-
-        </div>
-</div>
-
-
-<div class="container">
-        <div class="row">
-            <div class="col-lg-8 p-lg-2">
-                <div class="col-md">
-                    <div class="container mt-3 border-border border-0 p-3 rounded">
-                    <h2 class="p-2 text-dark"><?php the_field( 'trainingblock1title' );?></h2>
+                        <h2 class="p-2 text-dark"><?php the_field( 'trainingblock1title' );?></h2>
                             <!-- Post Block 1-->
                             <?php get_template_part('template-parts/block_1');?>
                             <!-- End of Post Block--> 
@@ -39,19 +26,17 @@
 </div>
 
 
-                        <div class="sidebar col-lg-4 col-md-4 p-2">
 
-                                      <!-- Category sidebar search-->
-                                      <?php get_template_part('includes/section','search');?>
-                                      <!-- End Category sidebar search-->
+<div class="sidebar col-lg-4 col-md-4 p-2">
 
-		   <!-- Start of posts sidebar related posts-->
-	 	   <?php get_template_part( 'template-parts/generic_sidebar' );?>
-		   <!-- End of posts sidebar related posts-->        
-                        </div>
-
-      </div>               
-
+<!-- Category sidebar search-->
+<?php get_template_part('includes/section','search');?>
+<!-- End Category sidebar search-->
+<!-- Start of posts sidebar related posts-->
+<?php get_template_part( 'template-parts/generic_sidebar' );?>
+<!-- End of posts sidebar related posts-->        
+</div>
+</div>               
 </div>
 
 <?php get_footer();?>
